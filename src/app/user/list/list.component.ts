9a3edb30e3b1832f77cecbee075b55a695edf843
imasha <chamodiimasha801@gmail.com>
2023-02-01 12:15:50 +0530

    minor fixes

--- src/app/user/list/list.component.ts
+++ src/app/user/list/list.component.ts
@@ -16,6 +16,7 @@ export class ListComponent implements OnInit {
   ngOnInit() {
     this.listService.getItems().subscribe((data: any) => {
       this.doctorList = data;
+      console.log("test");
     });
   }
 
